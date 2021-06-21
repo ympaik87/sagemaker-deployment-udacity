5656ada021d72f297be9f47c2731c7f0b327591d
Young Min Paik <ympaik@hotmail.com>
2021-06-21 11:52:32 +0900

    for uploading to sagemaker

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -3,17 +3,18 @@ import json
 import os
 import pickle
 import sys
-import sagemaker_containers
-import pandas as pd
+
 import numpy as np
+import pandas as pd
+import sagemaker_containers
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data
 
 from model import LSTMClassifier
+from utils import convert_and_pad, review_to_words
 
-from utils import review_to_words, convert_and_pad
 
 def model_fn(model_dir):
     """Load the PyTorch model from the `model_dir` directory."""
@@ -29,7 +30,8 @@ def model_fn(model_dir):
 
     # Determine the device and construct the model.
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = LSTMClassifier(model_info['embedding_dim'], model_info['hidden_dim'], model_info['vocab_size'])
+    model = LSTMClassifier(
+        model_info['embedding_dim'], model_info['hidden_dim'], model_info['vocab_size'])
 
     # Load the store model parameters.
     model_path = os.path.join(model_dir, 'model.pth')
@@ -46,38 +48,41 @@ def model_fn(model_dir):
     print("Done loading model.")
     return model
 
+
 def input_fn(serialized_input_data, content_type):
     print('Deserializing the input data.')
     if content_type == 'text/plain':
         data = serialized_input_data.decode('utf-8')
         return data
-    raise Exception('Requested unsupported ContentType in content_type: ' + content_type)
+    raise Exception(
+        'Requested unsupported ContentType in content_type: ' + content_type)
+
 
 def output_fn(prediction_output, accept):
     print('Serializing the generated output.')
     return str(prediction_output)
 
+
 def predict_fn(input_data, model):
     print('Inferring sentiment of input data.')
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    
+
     if model.word_dict is None:
         raise Exception('Model has not been loaded properly, no word_dict.')
-    
+
     # TODO: Process input_data so that it is ready to be sent to our model.
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
-
-    data_X = None
-    data_len = None
+    word_r2w = review_to_words(input_data)
+    data_X, data_len = convert_and_pad(model.word_dict, word_r2w)
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
     data_pack = np.hstack((data_len, data_X))
     data_pack = data_pack.reshape(1, -1)
-    
+
     data = torch.from_numpy(data_pack)
     data = data.to(device)
 
@@ -86,7 +91,9 @@ def predict_fn(input_data, model):
 
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
+    with torch.no_grad():
+        out = model.forward(data)
 
-    result = None
+    result = np.round(out.numpy())
 
     return result
--- Project/train/model.py
+++ Project/train/model.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 
+
 class LSTMClassifier(nn.Module):
     """
     This is the simple RNN model we will be using to perform Sentiment Analysis.
@@ -15,7 +16,7 @@ class LSTMClassifier(nn.Module):
         self.lstm = nn.LSTM(embedding_dim, hidden_dim)
         self.dense = nn.Linear(in_features=hidden_dim, out_features=1)
         self.sig = nn.Sigmoid()
-        
+
         self.word_dict = None
 
     def forward(self, x):
@@ -23,8 +24,8 @@ class LSTMClassifier(nn.Module):
         Perform a forward pass of our model on some input.
         """
         x = x.t()
-        lengths = x[0,:]
-        reviews = x[1:,:]
+        lengths = x[0, :]
+        reviews = x[1:, :]
         embeds = self.embedding(reviews)
         lstm_out, _ = self.lstm(embeds)
         out = self.dense(lstm_out)
